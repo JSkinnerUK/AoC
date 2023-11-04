1477e949d929b09955956dac1def0d45a92d579c
James Skinner <56730344+JSkinnerUK@users.noreply.github.com>
2023-11-04 03:58:36 +0000

    Day3 Part 1

--- Day03/Part1.c
+++ Day03/Part1.c
@@ -1,10 +1,49 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <strings.h>
 #include "../common/file_reader.h"
+
+int total = 0;
+
+char find_errors(char *comp1, char *comp2, int maxSize) {
+
+    for(int i = 0; i < strlen(comp1); i++) {
+        for(int j = 0; j < strlen(comp2); j++) {
+            if(comp1[i] == comp2[j]) {
+                return comp1[i];
+            }    
+        }
+    }
+    return '\0';
+}
 
 // Implement the line handler function
 void handle_line(const char *line)
 {
-    printf("%s", line);
+    size_t length = strlen(line) - 1;
+
+    char *comp1 = (char *)malloc(length / 2 + 1);
+    char *comp2 = (char *)malloc(length / 2 + 1);
+
+    strncpy(comp1, line, length / 2);
+    comp1[length / 2] = '\0';
+
+    strncpy(comp2, line + length / 2, length / 2);
+    comp2[length / 2] = '\0';
+    char x = find_errors(comp1, comp2, length / 2);
+
+    int asciiVal = x;
+    if (asciiVal >= 97) {
+        asciiVal -= 96;
+    } else {
+        asciiVal -= 64 - 26;
+    }
+
+    total+=asciiVal;
+
+    free(comp1);
+    free(comp2);
 }
 
 int main(int argc, char *argv[])
@@ -14,5 +53,6 @@ int main(int argc, char *argv[])
     // Call the function from the file_reader.c file
     read_file_lines(fileName, handle_line);
 
+    printf("%d\n", total);
     return 0;
 }
