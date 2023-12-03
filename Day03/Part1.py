a31a8c946d1c68875bf3d6e2e157fdde958a126b
James Skinner <56730344+JSkinnerUK@users.noreply.github.com>
2023-12-03 17:49:26 +0000

    Day3 Part1

---
+++ Day03/Part1.py
@@ -0,0 +1,40 @@
+import re
+
+total = 0
+
+def getSymbols(data):
+    symbols = set()
+    ignore = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'}
+    for line in data:
+        symbols.update(list(line))
+        symbols = symbols.difference(ignore)
+
+    return symbols
+
+def getNums(line):
+
+    x = re.finditer('[0-9]+', line)
+    y = [(m.start(0), m.end(0), m.group()) for m in x]
+    return y
+
+def checkContains(line, start, end, symbols):
+    subString = line[max(start-1, 0):min(end+1, len(line))]
+    return set(list(subString)).intersection(symbols)
+
+
+with open('Day03/input.txt', 'r') as file:
+    #Ignore the \n on each line
+    data = [line[:-1] for line in file]
+
+symbols = getSymbols(data)
+
+for i, line in enumerate(data):
+    nums = getNums(line)
+    for num in nums:
+        for j in range(-1,2):
+            if (i+j >= 0 and i+j < len(data)):
+                if checkContains(data[i+j], num[0], num[1], symbols):
+                    total += int(num[2])
+                    break
+
+print(total)            
